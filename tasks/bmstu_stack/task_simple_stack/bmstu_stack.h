96ceff54c724e5b4c345d71187d04ddf09f3b594
SheronR <uigor0839@gmail.com>
2025-12-19 10:41:39 +1100

    aft_clang-f

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -1,187 +1,199 @@
 #pragma once
 
-#include <exception>
-#include <iostream>
-#include <utility>
-#include <stdexcept>
-#include <initializer_list>
-#include <memory>
 #include <algorithm>
+#include <exception>
+#include <initializer_list>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <utility>
 
 namespace bmstu
 {
 template <typename T>
 class stack
 {
-public:
-    stack() : data_(nullptr), size_(0), capacity_(0) {}
+   public:
+	stack() : data_(nullptr), size_(0), capacity_(0) {}
 
-    explicit stack(size_t capacity) : data_(nullptr), size_(0), capacity_(0) 
-    {
-        if (capacity > 0) {
-            data_ = static_cast<T*>(::operator new(capacity * sizeof(T)));
-            capacity_ = capacity;
-        }
-    }
+	explicit stack(size_t capacity) : data_(nullptr), size_(0), capacity_(0)
+	{
+		if (capacity > 0)
+		{
+			data_ = static_cast<T*>(::operator new(capacity * sizeof(T)));
+			capacity_ = capacity;
+		}
+	}
 
-    stack(std::initializer_list<T> init) : data_(nullptr), size_(0), capacity_(0)
-    {
-        if (init.size() > 0) {
-            data_ = static_cast<T*>(::operator new(init.size() * sizeof(T)));
-            capacity_ = init.size();
-            for (const auto& item : init) {
-                new (&data_[size_++]) T(item);
-            }
-        }
-    }
+	stack(std::initializer_list<T> init)
+		: data_(nullptr), size_(0), capacity_(0)
+	{
+		if (init.size() > 0)
+		{
+			data_ = static_cast<T*>(::operator new(init.size() * sizeof(T)));
+			capacity_ = init.size();
+			for (const auto& item : init)
+			{
+				new (&data_[size_++]) T(item);
+			}
+		}
+	}
 
-    stack(const stack& other) : data_(nullptr), size_(other.size_), capacity_(other.capacity_)
-    {
-        if (capacity_ > 0) {
-            data_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
-            for (size_t i = 0; i < size_; ++i) {
-                new (&data_[i]) T(other.data_[i]);
-            }
-        }
-    }
+	stack(const stack& other)
+		: data_(nullptr), size_(other.size_), capacity_(other.capacity_)
+	{
+		if (capacity_ > 0)
+		{
+			data_ = static_cast<T*>(::operator new(capacity_ * sizeof(T)));
+			for (size_t i = 0; i < size_; ++i)
+			{
+				new (&data_[i]) T(other.data_[i]);
+			}
+		}
+	}
 
-    stack(stack&& other) noexcept 
-        : data_(other.data_), size_(other.size_), capacity_(other.capacity_)
-    {
-        other.data_ = nullptr;
-        other.size_ = 0;
-        other.capacity_ = 0;
-    }
+	stack(stack&& other) noexcept
+		: data_(other.data_), size_(other.size_), capacity_(other.capacity_)
+	{
+		other.data_ = nullptr;
+		other.size_ = 0;
+		other.capacity_ = 0;
+	}
 
-    stack& operator=(const stack& other)
-    {
-        if (this != &other) {
-            stack temp(other);
-            swap(temp);
-        }
-        return *this;
-    }
+	stack& operator=(const stack& other)
+	{
+		if (this != &other)
+		{
+			stack temp(other);
+			swap(temp);
+		}
+		return *this;
+	}
 
-    stack& operator=(stack&& other) noexcept
-    {
-        if (this != &other) {
-            clear();
-            ::operator delete(data_);
-            
-            data_ = other.data_;
-            size_ = other.size_;
-            capacity_ = other.capacity_;
-            
-            other.data_ = nullptr;
-            other.size_ = 0;
-            other.capacity_ = 0;
-        }
-        return *this;
-    }
+	stack& operator=(stack&& other) noexcept
+	{
+		if (this != &other)
+		{
+			clear();
+			::operator delete(data_);
 
-    ~stack() 
-    {
-        clear();
-        ::operator delete(data_);
-    }
+			data_ = other.data_;
+			size_ = other.size_;
+			capacity_ = other.capacity_;
 
-    bool empty() const noexcept 
-    { 
-        return size_ == 0; 
-    }
+			other.data_ = nullptr;
+			other.size_ = 0;
+			other.capacity_ = 0;
+		}
+		return *this;
+	}
 
-    size_t size() const noexcept 
-    { 
-        return size_; 
-    }
+	~stack()
+	{
+		clear();
+		::operator delete(data_);
+	}
 
-    template <typename... Args>
-    void emplace(Args&&... args)
-    {
-        if (size_ == capacity_) {
-            reserve(capacity_ == 0 ? 1 : size_ + 1);
-        }
-        new (&data_[size_++]) T(std::forward<Args>(args)...);
-    }
+	bool empty() const noexcept { return size_ == 0; }
 
-    void push(T&& value)
-    {
-        if (size_ == capacity_) {
-            reserve(capacity_ == 0 ? 1 : size_ + 1);
-        }
-        new (&data_[size_++]) T(std::move(value));
-    }
+	size_t size() const noexcept { return size_; }
 
-    void push(const T& value)
-    {
-        if (size_ == capacity_) {
-            reserve(capacity_ == 0 ? 1 : size_ + 1);
-        }
-        new (&data_[size_++]) T(value);
-    }
+	template <typename... Args>
+	void emplace(Args&&... args)
+	{
+		if (size_ == capacity_)
+		{
+			reserve(capacity_ == 0 ? 1 : size_ + 1);
+		}
+		new (&data_[size_++]) T(std::forward<Args>(args)...);
+	}
 
-    void pop()
-    {
-        if (empty()) {
-            throw std::underflow_error("Stack is empty");
-        }
-        --size_;
-        data_[size_].~T();
-    }
+	void push(T&& value)
+	{
+		if (size_ == capacity_)
+		{
+			reserve(capacity_ == 0 ? 1 : size_ + 1);
+		}
+		new (&data_[size_++]) T(std::move(value));
+	}
 
-    T& top()
-    {
-        if (empty()) {
-            throw std::underflow_error("Stack is empty");
-        }
-        return data_[size_ - 1];
-    }
+	void push(const T& value)
+	{
+		if (size_ == capacity_)
+		{
+			reserve(capacity_ == 0 ? 1 : size_ + 1);
+		}
+		new (&data_[size_++]) T(value);
+	}
 
-    const T& top() const
-    {
-        if (empty()) {
-            throw std::underflow_error("Stack is empty");
-        }
-        return data_[size_ - 1];
-    }
+	void pop()
+	{
+		if (empty())
+		{
+			throw std::underflow_error("Stack is empty");
+		}
+		--size_;
+		data_[size_].~T();
+	}
 
-    void clear() noexcept
-    {
-        for (size_t i = 0; i < size_; ++i) {
-            data_[i].~T();
-        }
-        size_ = 0;
-    }
+	T& top()
+	{
+		if (empty())
+		{
+			throw std::underflow_error("Stack is empty");
+		}
+		return data_[size_ - 1];
+	}
 
-    void swap(stack& other) noexcept
-    {
-        using std::swap;
-        swap(data_, other.data_);
-        swap(size_, other.size_);
-        swap(capacity_, other.capacity_);
-    }
+	const T& top() const
+	{
+		if (empty())
+		{
+			throw std::underflow_error("Stack is empty");
+		}
+		return data_[size_ - 1];
+	}
 
-private:
-    T* data_;
-    size_t size_;
-    size_t capacity_;
+	void clear() noexcept
+	{
+		for (size_t i = 0; i < size_; ++i)
+		{
+			data_[i].~T();
+		}
+		size_ = 0;
+	}
 
-    void reserve(size_t new_capacity)
-    {
-        if (new_capacity <= capacity_) {
-            return;
-        }
+	void swap(stack& other) noexcept
+	{
+		using std::swap;
+		swap(data_, other.data_);
+		swap(size_, other.size_);
+		swap(capacity_, other.capacity_);
+	}
 
-        T* new_data = static_cast<T*>(::operator new(new_capacity * sizeof(T)));
-        
-        for (size_t i = 0; i < size_; ++i) {
-            new (&new_data[i]) T(std::move(data_[i]));
-            data_[i].~T();
-        }
-        
-        ::operator delete(data_);
-        data_ = new_data;
-        capacity_ = new_capacity;
-    }
+   private:
+	T* data_;
+	size_t size_;
+	size_t capacity_;
+
+	void reserve(size_t new_capacity)
+	{
+		if (new_capacity <= capacity_)
+		{
+			return;
+		}
+
+		T* new_data = static_cast<T*>(::operator new(new_capacity * sizeof(T)));
+
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (&new_data[i]) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+
+		::operator delete(data_);
+		data_ = new_data;
+		capacity_ = new_capacity;
+	}
 };
 }  // namespace bmstu
